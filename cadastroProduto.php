8f291c697af1142e453d7536f502474c182de830
NATHALY LOUISE LESCZK CUNHA <nathy.zulmira.com>
2025-11-17 16:36:58 -0300

    commit inicial

---
+++ cadastroProduto.php
@@ -0,0 +1,29 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="pt-br">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar Produtos</title>
+</head>
+<body>
+    <form action="" method="post">
+        <h2>Cadastrar Produtos</h2>
+
+        <label for="nome"></label>
+        <input type="text" name="nome"><br>
+
+        <label for="preco"></label>
+        <input type="number" name="preco"><br>
+
+        <label for="quantidade"></label>
+        <input type="number" name="quantidade"><br>
+
+        <label for="descricao"></label>
+        <input type="text" name="descricao"><br>
+
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+$host = "localhost";
+$banco = "sistema_produtos";
+$usuario = "root";
+$senha = "";
+
+try{
+    $pdo = new PDO("mysql:host=$host;dbname=$banco;harset=utf8", $usuario, $senha);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}  catch(PDOException $e){
+    echo "Erro ao conectar: ". $e->getMessage();
+}
+
+?>
